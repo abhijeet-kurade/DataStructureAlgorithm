215660fe7e903049c2f80aa916e7b1d10bb6f54f
Abhijeet B Kurade <64726465+abhijeet-kurade@users.noreply.github.com>
2024-03-10 13:08:19 +0530

    Segment tree

---
+++ src/SegmentTree/RangeQueryForMaxNumberUsingNodeClass.java
@@ -0,0 +1,129 @@
+package SegmentTree;
+
+public class RangeQueryForMaxNumberUsingNodeClass {
+    public static void main(String[] args) {
+
+        int[] arr = {221,215,87,45,22,123,567,213,33};
+        int[][]  queries   = {{0,8},{3,5},{2,5},{3,6}};
+        getMaxInRange(arr,  queries);
+    }
+
+    public static int[] getMaxInRange(int[] arr, int[][] queries){
+
+        Node root = buildSegmentTree(arr, 0, arr.length-1);
+
+        int[] answer   = new int[queries.length];
+
+
+        int idx = 0;
+        for(int[]  query :  queries){
+            int left = query[0], right = query[1];
+            answer[idx] =  getMaxInRageInSegmentTree(root, left, right);
+            idx += 1;
+        }
+        return answer;
+    }
+
+    public static int getMaxInRageInSegmentTree(Node node, int left, int  right){
+        if(right < node.min || node.max <  left) return Integer.MIN_VALUE;
+
+        if(left  <= node.min && node.max <= right ){
+            return node.val;
+        }
+
+        int l = getMaxInRageInSegmentTree(node.left, left,  right);
+        int r = getMaxInRageInSegmentTree(node.right, left,  right);
+
+        return Math.max(l, r);
+    }
+
+    public static Node buildSegmentTree(int[]  arr, int left, int right){
+        if(left > right) return null;
+        Node node = new Node(left, right);
+        if(left == right){
+            node.val = arr[left];
+            return node;
+        }
+        int mid = left + (right - left ) / 2;
+        node.left = buildSegmentTree(arr, left, mid);
+        node.right = buildSegmentTree(arr, mid+1, right);
+        node.val = Math.max(node.left != null ? node.left.val : Integer.MIN_VALUE, node.right != null ? node.right.val : Integer.MIN_VALUE);
+        return node;
+    }
+
+    public static class Node{
+        public int val;
+        int min, max;
+        public Node left, right;
+
+        public Node(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
+
+/* The functions which
+builds the segment tree */
+class GfG
+{
+    public static Node root;
+    public static class Node{
+        public int val;
+        int min, max;
+        public Node left, right;
+
+        public Node(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static Node buildSegmentTree(int[]  arr, int left, int right){
+        if(left > right) return null;
+        Node node = new Node(left, right);
+        if(left == right){
+            node.val = arr[left];
+            return node;
+        }
+        int mid = left + (right - left ) / 2;
+        node.left = buildSegmentTree(arr, left, mid);
+        node.right = buildSegmentTree(arr, mid+1, right);
+        node.val = Math.min(node.left != null ? node.left.val : Integer.MAX_VALUE, node.right != null ? node.right.val : Integer.MAX_VALUE);
+        return node;
+    }
+
+    public static int getMinInRageInSegmentTree(Node node, int left, int  right){
+        if(right < node.min || node.max <  left) return Integer.MAX_VALUE;
+
+        if(left  <= node.min && node.max <= right ){
+            return node.val;
+        }
+
+        int l = getMinInRageInSegmentTree(node.left, left,  right);
+        int r = getMinInRageInSegmentTree(node.right, left,  right);
+
+        return Math.min(l, r);
+    }
+
+    static int st[];
+
+    public static int[] constructST(int arr[], int n)
+    {
+        // Add your code here
+        root =  buildSegmentTree(arr, 0,  arr.length-1);
+        return null;
+    }
+
+
+    /* The functions returns the
+      min element in the range
+      from l and r */
+    public static int RMQ(int st[], int n, int l, int r)
+    {
+        // Add your code here
+        return getMinInRageInSegmentTree(root, l, r);
+    }
+
+
+}
---
+++ src/SegmentTree/WithClass/SegmentTree.java
@@ -0,0 +1,68 @@
+package SegmentTree.WithClass;
+
+public class SegmentTree {
+    class Node {
+        int val;
+        int min, max;
+        Node left, right;
+
+        public Node() {
+        }
+
+        public Node(int val, int min, int max) {
+            this.val = val;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    Node root;
+
+    public SegmentTree(int[] arr){
+        this.root = constructSegmentTree(arr, 0, arr.length-1);
+    }
+
+    public Node constructSegmentTree(int[] arr, int min, int max){
+        if(min == max){
+            Node node = new Node(arr[max], min, max);
+            return node;
+        }
+        int mid = min + (max - min) / 2;
+        Node node = new Node();
+        node.left = constructSegmentTree(arr, min,mid);
+        node.right = constructSegmentTree(arr, mid+1, max);
+        node.val = node.left.val + node.right.val;
+        return node;
+    }
+
+    public void set(int idx, int val){
+        set(this.root, idx, val);
+    }
+
+    private void set(Node node, int idx, int val){
+        if(node.min == node.max && node.min == idx){
+            node.val = val;
+            return;
+        }
+        if(idx<node.min || node.max < idx) {
+            return;
+        }
+        set(node.left, idx, val);
+        set(node.left, idx, val);
+        node.val = node.left.val + node.right.val;
+        return;
+    }
+
+    public int rangeSum(int min, int max){
+        return rangeSum(this.root, min, max);
+    }
+    private int rangeSum(Node node, int min, int max){
+        if(min <= node.min &&  node.max <= max){
+            return node.val;
+        }
+        if( max < node.min || node.max < min){
+            return 0;
+        }
+        int sum = rangeSum(node.left, min, max) + rangeSum(node.right, min, max);
+        return sum;
+    }
+}
